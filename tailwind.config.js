808b356a1ab6aaddb1c32e6c6ff5ff84d6133662
allan <allan.gtm92@gmail.com>
2025-02-17 13:15:04 +0545

    feat: update dependencies and configure Tailwind CSS with Material Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = withMT({
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        fondamento: ['Fondamento', 'sans-serif'],
-        fondamentoItalic: ['Fondamento-Italic', 'sans-serif'],
-        montserrat: [ 'Montserrat', 'sans-serif'],
-        montserratItalic: [ 'Montserrat-Italic', 'sans-serif'],
+        fondamento: ["Fondamento", "sans-serif"],
+        fondamentoItalic: ["Fondamento-Italic", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+        montserratItalic: ["Montserrat-Italic", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
+  darkMode: "class",
+});
